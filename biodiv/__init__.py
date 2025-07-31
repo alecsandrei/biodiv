d23e495e469397da054ef5cb61d4e0a574daf8e1
Alex Andrei Cuvuliuc <cuvuliucalexandrei@gmail.com>
2025-07-31 22:51:26 +0300

    use Qt5Agg in matplotlib

--- biodiv/__init__.py
+++ biodiv/__init__.py
@@ -1,3 +1,17 @@
+from __future__ import annotations
+
+import numpy as np
+from loguru import logger
+
 from biodiv import config
 
-__all__ = ["config"]
+__all__ = ['config']
+
+try:
+    import matplotlib
+
+    matplotlib.use('Qt5Agg')
+except Exception as e:
+    logger.warning(
+        'Failed to import and configure matplotlib to use Qt6Agg. Error: %s' % e
+    )
