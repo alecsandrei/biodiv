8d1685803f34e0e452c639c072e3d0c86ffd6271
Alex Andrei Cuvuliuc <cuvuliucalexandrei@gmail.com>
2025-08-03 20:23:17 +0300

    remove Qt5Agg call and warning filters

--- biodiv/__init__.py
+++ biodiv/__init__.py
@@ -1,23 +1,7 @@
 from __future__ import annotations
 
-import warnings
-
 import numpy as np
-from loguru import logger
 
 from biodiv import config
 
-__all__ = ['config']
-
-try:
-    import matplotlib
-
-    matplotlib.use('Qt5Agg')
-except Exception as e:
-    logger.warning(
-        'Failed to import and configure matplotlib to use Qt6Agg. Error: %s' % e
-    )
 np.random.seed(config.RANDOM_SEED)
-# Silences pkg_resources deprecation warning
-warnings.filterwarnings('ignore', category=UserWarning)
-warnings.filterwarnings('ignore', category=DeprecationWarning)
